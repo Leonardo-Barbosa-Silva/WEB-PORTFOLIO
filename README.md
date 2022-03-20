f8a93ece0a0e391eb33d863e78dc0d03a3dfc215
unknown <leonardobarbosassilva@gmail.com>
2022-03-20 16:00:27 -0300

    :package: contact

---
+++ README.md
@@ -0,0 +1,11 @@
+# WEB-PORTFOLIO
+Projeto de portfólio
+
+## TAREFAS
+As tarefas serão definidas no próprio "project" do GitHub
+
+## Ícones
+:package: Nova funcionalidade
+:up: Atualização
+:beetle: Correção de Bugs
+:checkered_flag: Release
---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelector(".hamburguer").addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("show-menu")
+})
+
+document.querySelector("#menu-item-home a").addEventListener("mouseover", () => {
+    let lineHome = document.querySelector(".menu div")
+    lineHome.style.backgroundColor = "rgb(150, 0, 150)"
+})
+
+document.querySelector("#menu-item-home a").addEventListener("mouseout", () => {
+    let lineHome = document.querySelector(".menu div")
+    lineHome.style.backgroundColor = "#000"
+})
